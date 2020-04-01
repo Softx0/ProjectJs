666f062eea98886811c386b2caaff8d6103eeaf2
Eduardo <ing.valen0421@hotmail.com>
2020-04-01 18:35:51 -0400

    modifies done!

--- Proyecto/Scripts/retrievingSolicitud.js
+++ Proyecto/Scripts/retrievingSolicitud.js
@@ -1,7 +1,15 @@
-var data = [];
-var inputs = document.getElementsByTagName('input');
-for(var i=0; i< inputs.length; i++)
-{
-  data.push(inputs[i].value);
+let data = new Array(); // creating array
+
+function add_element(){
+    data.push(document.getElementById('cedula').value); // adding element to array
+    document.getElementById('cedula').value=''; // Making the text box blank
+    display(); // displaying the array elements
 }
-console.log(data);
+
+function display(){
+    let str = "";
+    for (i=0;i<data.length;i++){ 
+        str += data[i] + "<br >";
+    }
+    document.getElementById('disp').innerHTML = str;
+}
