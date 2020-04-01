604f4704b09535a7b84ad36918b66987ab4dc982
Eduardo <ing.valen0421@hotmail.com>
2020-04-01 18:11:52 -0400

    Done

--- Proyecto/Scripts/retrievingSolicitud.js
+++ Proyecto/Scripts/retrievingSolicitud.js
@@ -1,29 +1,7 @@
-<<<<<<< master
-<<<<<<< HEAD
-let myArray = [];
-let formInputs = document.getElementsByClassName('myInput');
-
-let formData = Array.prototype.filter.call(formInputs, function(element){
-    return console.log(element.value);
-});
-=======
-// let myArray = [];
-// let formInputs = document.getElementsByClassName('myInput');
-
-// let formData = Array.prototype.filter.call(formInputs, function(element){
-//     return console.log(element.value);
-// });
-
 var data = [];
 var inputs = document.getElementsByTagName('input');
 for(var i=0; i< inputs.length; i++)
-=======
-let data = [];
-let inputs = document.getElementsByTagName('input');
-for(let i=0; i< inputs.length; i++)
->>>>>>> modify js file
 {
   data.push(inputs[i].value);
 }
 console.log(data);
->>>>>>> Emmanuel
