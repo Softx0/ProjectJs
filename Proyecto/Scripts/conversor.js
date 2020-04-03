40dd9566cded75dab1fa0d2a76b7ab2bb5bdc18a
Eduardo <ing.valen0421@hotmail.com>
2020-04-03 15:30:07 -0400

    Conversor done

--- Proyecto/Scripts/conversor.js
+++ Proyecto/Scripts/conversor.js
@@ -1,17 +1,38 @@
+let select;
+let selectedValueDesde;
+let valueInput;
+
 function convertirGrados(){
-    let e = document.getElementById("conversor-grados-desde");
-    let value = e.options[e.selectedIndex].value;
-    let text = e.options[e.selectedIndex].text;
 
-    console.log(value, text);
+    select = document.getElementById('conversor-grados'); 
+    selectedValueDesde = Number(select.options[select.selectedIndex].value); 
+    valueInput = Number(document.getElementById('conversor').value);
 
-    let r = document.getElementById("conversor-grados-hasta");
-    let value1 = r.options[r.selectedIndex].value;
-    let text1 = r.options[r.selectedIndex].text;
-
-    console.log(value1, text1);
-    let str = text + " " + text1;
-    return str;
+    switch (selectedValueDesde) {
+        case 1: 
+            valueInput = (valueInput * 9/5) + 32;
+            console.log('valueInput', valueInput);
+            break;
+        case 2:
+            valueInput += 273.15;
+            console.log('celsius a kelvin', valueInput);
+            break;
+        case 3:
+            valueInput = (valueInput - 32) * 5/9;
+            console.log('farenheit  celsius', valueInput);
+            break;
+        case 4:
+            valueInput = (valueInput - 32) * 5/9 + 273.15;
+            console.log('farenheit a kelvin', valueInput);
+            break;
+        case 5:
+            valueInput -= 273.15;
+            console.log('kelvin a celsius', valueInput);
+            break;
+        default:
+            valueInput = (valueInput - 273.15) * 9/5 + 32;
+            console.log('kelvin a farenheit', valueInput);
+            break;
+    } 
 }
 
-document.getElementById('valores').innerHTML = convertirGrados();
