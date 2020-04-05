802b5c2c034b95235c72a29a3610be548550f8d8
Shirley Contreras <shirleycontreras07gmail.com>
2020-04-04 21:04:08 -0400

    Suma and resta function done calculadora.js

--- Proyecto/Scripts/calculadora.js
+++ Proyecto/Scripts/calculadora.js
@@ -1,1 +1,30 @@
+//Se obtienen los dos valores ingresados
+let primerValor = 0;
+let segundoValor = 0;
 
+
+function getValores() {
+    primerValor = Number(document.getElementById("primerValor").value);
+    segundoValor = Number(document.getElementById("segundoValor").value);
+}
+
+function sumar() {
+   
+    suma = primerValor + segundoValor;
+    console.log(suma);
+    document.getElementById("resultado").innerHTML = "La sumatoria de estos valores es: " + suma;
+}
+
+function restar() {
+    
+    resta = primerValor - segundoValor;
+    console.log(resta);
+    document.getElementById("resultado").innerHTML = "La resta de estos valores es: " + resta;
+}
+
+function multiplicar() {
+    
+    suma = primerValor + segundoValor;
+    console.log(suma);
+    document.getElementById("resultado").innerHTML = "La sumatoria de estos valores es: " + suma;
+}
